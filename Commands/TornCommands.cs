facde0644342b763440b96dc9607dbf02b0c4db5
L4Leezus <35348495+L4Leezus@users.noreply.github.com>
2024-09-06 10:39:13 +0100

    Add files via upload
    All files minus packages:
    DSharpPlus

---
+++ Commands/TornCommands.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading.Tasks;
+
+namespace TornWarTracker.Commands
+{
+    public class TornCommands : BaseCommandModule
+    {
+        [Command("Hi")]
+        public async Task TestCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync($"Hello {ctx.User.Username}");
+
+        }
+
+    }
+}
---
+++ config/JSONReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TornWarTracker.config
+{
+    public class JSONReader
+    {
+        public string token {  get; set; }
+        public string prefix { get; set; }
+        public async Task ReadJson()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(json);
+                this.token = data.token;
+                this.prefix = data.prefix;
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+
+}
